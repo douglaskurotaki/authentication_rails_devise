0111bad57b69d0b74e55e7e816cf6dc1507254a8
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2021-04-19 19:43:14 -0300

    fix deploy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,4 +70,19 @@ Rails.application.configure do
 
   # Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # email
+  config.action_mailer.default_url_options = {
+    host: 'course-blog-rails-api.herokuapp.com'
+  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: Rails.application.credentials.gmail[:user_name].to_s,
+    password: Rails.application.credentials.gmail[:password],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
